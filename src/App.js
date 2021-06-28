06f2201cd4e25fd9cc00e8c1a6cab9193f5e5a81
@victor.ortiz1 <victor.ortiz@improvein.com>
2021-06-27 23:07:31 -0300

    deploy 2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Links from "./components/Links";
+import "./App.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "firebase/auth";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+ 
+} from "react-router-dom";
+import { User } from "./components/User";
+import { Navbar_2 } from "./components/Navbar";
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
+import DomingoListPrimera from "./components/DomingoListPimera";
+import DomingoListSegunda from "./components/DomingoListSegunda";
+import SabadoListPimera from "./components/SabadoListPimera";
+import SabadoListSegunda from "./components/SabadoListSegunda";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar_2/>
+      <Switch>
+        <Route exact path="/">
+        <div className="container p-4">
+        <div className="row">
+          <Links />
+        </div>
+        <ToastContainer />
+      </div>
+        </Route>
+        <Route exact path="/user">
+          <User />
+        </Route>
+        <Route exact path="/sabado-primera">
+          <SabadoListPimera />
+        </Route>
+        <Route exact path="/sabado-segunda">
+          <SabadoListSegunda />
+        </Route>
+        <Route exact path="/domingo-primera">
+          <DomingoListPrimera />
+        </Route>
+        <Route exact path="/domingo-segunda">
+          <DomingoListSegunda />
+        </Route>
+      </Switch>
+
+     
+    </Router>
   );
 }
 

e89063ea119cc5f8327675aacb47c7656e34ab46
@victor.ortiz1 <victor.ortiz@improvein.com>
2021-06-27 23:13:48 -0300

    Updates

---
+++ precache-manifest.e5dd90e35c4a2847bc2dcf47eae0f953.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf4a9dff919db76a0e695dbdda153149",
+    "url": "/planilla-iepe/index.html"
+  },
+  {
+    "revision": "43230d938ac430097eff",
+    "url": "/planilla-iepe/static/css/2.0cd25043.chunk.css"
+  },
+  {
+    "revision": "49ab463b53c27f47ed74",
+    "url": "/planilla-iepe/static/css/main.1689b37b.chunk.css"
+  },
+  {
+    "revision": "43230d938ac430097eff",
+    "url": "/planilla-iepe/static/js/2.9d210ee1.chunk.js"
+  },
+  {
+    "revision": "a2334ff9bc68c321bce67af5ebb4ec33",
+    "url": "/planilla-iepe/static/js/2.9d210ee1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49ab463b53c27f47ed74",
+    "url": "/planilla-iepe/static/js/main.757f16bc.chunk.js"
+  },
+  {
+    "revision": "fb3695301ff6147ff68e",
+    "url": "/planilla-iepe/static/js/runtime-main.9e313365.js"
+  }
+]);
